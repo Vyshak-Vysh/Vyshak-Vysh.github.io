eb6d3345b154ad61770d1ec07dd756009c2a0cd4
Vyshak-Vysh <121019560+Vyshak-Vysh@users.noreply.github.com>
2026-02-11 18:32:24 +0530

    changes

--- src/data/projects/archviz-proj.js
+++ src/data/projects/archviz-proj.js
@@ -21,15 +21,15 @@ export default {
       // description: '<a href="https://drive.google.com/drive/folders/1KX1zORN5uA7MWT8IxcBJK8I0O87kaARy?usp=sharing" target="_blank" rel="noopener noreferrer" class="drive-link" title="Archviz Model"><svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg><span class="drive-link-text">Archviz Model</span></a>'
     },
     secondaryArt: {
-      type: 'image',
-      src: 'images/publications/archviz/secondary.jpg',
-      alt: 'Interior Design Walkthrough',
-      description: '',
-      // bottomText: 'Minimap functionality',
-      controls: true,
-      autoplay: true,
-      loop: true,
-      muted: true
+      type: 'video',
+        src: '/images/UE_Dubai/Interior_Design.mp4',
+        alt: 'Interior Design Walkthrough',
+        description: '',
+        // bottomText: 'Minimap functionality',
+        controls: true,
+        autoplay: true,
+        loop: true,
+        muted: true
     },
     summaryHtml: `
       <h3>Overview</h3>
